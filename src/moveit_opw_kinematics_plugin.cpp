d2e6ac735f0e073b87da461271d78787cc5ebd42
jeroendm <jeroendemaeyer@live.be>
2018-07-30 15:53:30 +0200

    fix errors in getPositionIK for all joint solutions

--- src/moveit_opw_kinematics_plugin.cpp
+++ src/moveit_opw_kinematics_plugin.cpp
@@ -274,7 +274,7 @@ bool MoveItOPWKinematicsPlugin::searchPositionIK(const std::vector<geometry_msgs
 
 bool MoveItOPWKinematicsPlugin::getPositionIK(const std::vector<geometry_msgs::Pose> &ik_poses,
                                               const std::vector<double> &ik_seed_state,
-                                              std::vector<std::vector<double>> &solutions,
+                                              std::vector<std::vector<double> > &solutions,
                                               KinematicsResult &result,
                                               const kinematics::KinematicsQueryOptions &options) const
 {
